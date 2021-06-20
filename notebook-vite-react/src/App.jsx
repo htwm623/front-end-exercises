454cfe18b268707fae65dca99aee74436c998325
htwm623 <htwm623@outlook.com>
2021-06-21 00:27:10 +0800

    添加react-router-dom

--- notebook-vite-react/src/App.jsx
+++ notebook-vite-react/src/App.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+import './App.css';
+
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className='App'>
+      <Router>
+        <div>
+          <Switch>
+            <Route path='/'>
+              <Home></Home>
+            </Route>
 
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
